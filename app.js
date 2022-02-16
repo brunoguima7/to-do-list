82fbeb2445bffb5e31a66908cd417279df4abffa
Bruno Guimarães <brunodecamargoguimaraes@gmail.com>
2022-02-16 01:07:17 -0300

    Aula 02

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require ('express')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h1>Minha lista de tarefas</h1>")
+})
+
+app.get("/json", (req, res) => {
+    res.json({task: "Tarefa", done: true})
+})
+
+app.listen(3001, () => {
+    console.log("Servidor foi iniciado")
+})
