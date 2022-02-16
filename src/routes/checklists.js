2ec3126061776d246b5e3d4b244f15e16e77a071
Bruno Guimarães <brunodecamargoguimaraes@gmail.com>
2022-02-16 02:30:41 -0300

    Aula 04

--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -12,7 +12,22 @@ router.get("/json", (req, res) => { /*cria um arquivo no tipo json na url indica
 
 router.post("/", (req, res) => {
     console.log(req.body)
-    res.status(200).send(req.body)
+    res.send(req.body)
+})
+
+router.get("/:id", (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put("/:id", (req, res) => {
+    console.log(req.params.id)
+    res.send(`PUT: ${req.params.id}`)
+})
+
+router.delete("/:id", (req, res) => {
+    console.log(req.params.id)
+    res.send(`DELETE: ${req.params.id}`)
 })
 
 module.exports = router
