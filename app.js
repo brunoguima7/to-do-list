339ed6c03bde2670eb4ece07f663dbb102aab2ef
Bruno Guimarães <brunodecamargoguimaraes@gmail.com>
2022-02-26 03:35:15 -0300

    Aula 07

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require ("express")
 const checklistsRouter = require ("./src/routes/checklists")
 const rootRouter = require ("./src/routes/index")
 const path = require("path")
+const methodOverride = require("method-override")
 require("./config/database")
 
 const app = express()
 app.use(express.json()) /*middleware que faz com que arquivos json sejam lidos como json e não como tipo texto*/ 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method", {methods: ["POST", "GET"]}))
 
 app.use(express.static(path.join(__dirname, "public")))
 
@@ -13,7 +16,7 @@ app.set("views", path.join(__dirname, "src/views"))
 app.set("view engine", "ejs")
 
 app.use("/", rootRouter)
-app.use("/checklist", checklistsRouter)
+app.use("/checklists", checklistsRouter)
 
 app.listen(3001, () => {
     console.log("Servidor foi iniciado")
